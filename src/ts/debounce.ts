763b082971116548ad42766bef041a1250b2dca1
Carles Pueyo <cpcastells@gmail.com>
2023-09-19 12:38:53 +0200

    Create debounce function

---
+++ src/ts/debounce.ts
@@ -0,0 +1,15 @@
+const debounce = <T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+) => {
+  let timeoutId: NodeJS.Timeout;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+export default debounce;
