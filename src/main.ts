71e8dc77f4e727f18a24f295de41576b550f0747
Carles Pueyo <cpcastells@gmail.com>
2023-09-20 11:41:30 +0200

    Add functionality to throttle section

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
+import { throttle } from "./ts/throttle.js";
+
 // Debounce elements
 const input = document.getElementById("debounce-name") as HTMLInputElement;
 const normalValue = document.querySelector(".debounce-normal")!;
 const debouncedValue = document.querySelector(".debounce-effect")!;
+
+// Throttle elements
+const inputThrottle = document.getElementById(
+  "throttle-name",
+) as HTMLInputElement;
+const inputValueThrottle = document.querySelector(".throttle-normal")!;
+const throttledValue = document.querySelector(".throttle-effect")!;
 
 let debounceTimer: NodeJS.Timeout;
 
@@ -13,6 +22,14 @@ const updateDebounceValue = () => {
   debouncedValue.innerHTML = input.value;
 };
 
+const updateThrottleInputValue = () => {
+  inputValueThrottle.innerHTML = inputThrottle.value;
+};
+
+const updateThrottleThrottledValue = () => {
+  throttledValue.innerHTML = inputThrottle.value;
+};
+
 const debounce = (callback: () => void, delay: number) => {
   clearTimeout(debounceTimer);
   debounceTimer = setTimeout(callback, delay);
@@ -22,3 +39,13 @@ input.addEventListener("input", () => {
   updateInputValue();
   debounce(updateDebounceValue, 500);
 });
+
+inputThrottle.addEventListener(
+  "input",
+  () => {
+    updateThrottleInputValue();
+    const throttledUpdate = throttle(updateThrottleThrottledValue, 2000);
+    throttledUpdate();
+  },
+  false,
+);
